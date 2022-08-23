88e6389ceb4052f9e7e5300e17e9fc50ea68f952
phphlx <phphlx@163.com>
2022-08-23 11:28:10 +0800

    switch env

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import store from '@/store'
 import wepy from '@wepy/core'
 
 // 服务器接口地址
-const host = 'http://larabbs.test/api/v1/'
+const host = API_URL
 
 const checkToken = async () => {
   // 从缓存中取出 Token
